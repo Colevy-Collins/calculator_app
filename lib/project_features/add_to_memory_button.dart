63f18120afaaa4a37cf962b55fcff62ebff332c3
Pepper <56687049+echocall@users.noreply.github.com>
2024-10-25 18:42:26 -0400

    Update add_to_memory_button.dart
    - Added in ability to add userAnswer to the list.
    TODO:
     Move List<String>answerMemory to better location within program (probably main)
     Fix List<String> answerMemory declaration at some point.

--- lib/project_features/add_to_memory_button.dart
+++ lib/project_features/add_to_memory_button.dart
@@ -1,2 +1,18 @@
 // Adds the current userAnswer to a list for later retrieval.
 // Where to put list for further reach? Into main.dart?
+import '../base_project/command.dart';
+
+class MemoryAddCommand implements Command {
+  @override
+  List<String> execute(String userQuestion, String userAnswer) {
+    String question = userQuestion;
+    String answer = userAnswer;
+
+     List<String> answerMemory = new List<String>();
+    
+      answerMemory.add(userAnswer);
+
+    return [question, answer];
+
+  }
+}
