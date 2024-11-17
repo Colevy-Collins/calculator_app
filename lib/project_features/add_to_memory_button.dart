37d93accc7ab72f8140e9334255e1674f6b3c18d
Pepper <56687049+echocall@users.noreply.github.com>
2024-11-17 09:34:28 -0500

    Summary
    add_to_memory_dart
    - just fixed a casting issue.
    
    add_to_memory_button_test.dart
    - Adding testing for verifying initial state of answerMemory
    - Added testing for the execute Method within MemoryAddCommand
    - Added testing related to the Shared State which uncovered problems within testing context.
    - Added a tearDown to deal with the behavior of answerMemory staying between Tests.

--- lib/project_features/add_to_memory_button.dart
+++ lib/project_features/add_to_memory_button.dart
@@ -1,8 +1,8 @@
 import '../base_project/command.dart';
 
 class MemoryAddCommand implements Command {
-   static final answerMemory = List.empty(growable: true);
-   
+   static final List<String> answerMemory = List.empty(growable: true);
+
   @override
   List<String> execute(String userQuestion, String userAnswer) {
     String question = userQuestion;
